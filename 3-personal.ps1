1bb1b1587efd878190e5c64017e6d1b99a1b0d0d
Ainara Serna Méndez <117570048+AinaraSerna@users.noreply.github.com>
2026-01-15 14:22:41 +0100

    Fix user creation script and update domain path

--- 3-personal.ps1
+++ 3-personal.ps1
@@ -3,12 +3,13 @@ Set-Location "F:\Ensayos 2"
 $personal = Import-Csv ".\personal.csv"
 foreach($user in $personal){
     $login=$user.login
-    $path = "OU="+($user.departamento)+",OU="+($user.seccion)+",OU=ASM2122,DC=AINARA2021,DC=ES"
+    $path = "OU="+($user.departamento)+",OU="+($user.seccion)+",OU=ASM2122,DC=AINARA,DC=ES"
     New-ADUser -Name $login -Path $path -SamAccountName $login -UserPrincipalName ($login+"@AINARA.ES") `
-    -AccountPassword (ConvertTo-SecureString "AINARA2021.es" -AsPlainText -Force) `
+    -AccountPassword (ConvertTo-SecureString "AINARA.es" -AsPlainText -Force) `
     -GivenName $login -ChangePasswordAtLogon $false -Enabled $true
 
-    $group = Get-ADGroup ("CN="+$user.departamento+",OU="+$user.departamento+",OU="+$user.seccion+",OU=ASM2122,DC=AINARA2021,DC=ES")
+    $group = Get-ADGroup ("CN="+$user.departamento+",OU="+$user.departamento+",OU="+$user.seccion+",OU=ASM2122,DC=AINARA,DC=ES")
     $member = "CN="+$login+","+$path
     Add-ADGroupMember $group -Members $member
+
 }
