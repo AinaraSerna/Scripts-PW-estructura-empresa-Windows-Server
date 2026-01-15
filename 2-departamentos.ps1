eac02d2d3eb06379822a9c499a62526fa3223304
Ainara Serna Méndez <117570048+AinaraSerna@users.noreply.github.com>
2026-01-15 14:14:54 +0100

    Scripts PS! y archivos CSV

---
+++ 2-departamentos.ps1
@@ -0,0 +1,13 @@
+﻿
+Set-Location "F:\Ensayos 2"
+$departamentos = Import-Csv ".\departamentos.csv"
+foreach($ou in $departamentos){
+    $path="OU="+($ou.seccion)+",OU=ASM2122,DC=AINARA2021,DC=ES"
+    New-ADOrganizationalUnit -Name ($ou.departamento) -Path $path
+    $path="OU="+($ou.departamento)+",OU="+$ou.seccion+",OU=ASM2122,DC=AINARA2021,DC=ES"
+
+    New-ADGroup -Name $ou.departamento -GroupScope Global -Path $path
+    $group = Get-ADGroup ("CN="+$ou.seccion+",OU="+$ou.seccion+",OU=ASM2122,DC=AINARA2021,DC=ES")
+    $member="CN="+($ou.departamento)+","+$path
+    Add-ADGroupMember $group -Members $member
+}
---
+++ 3-personal.ps1
@@ -0,0 +1,14 @@
+﻿
+Set-Location "F:\Ensayos 2"
+$personal = Import-Csv ".\personal.csv"
+foreach($user in $personal){
+    $login=$user.login
+    $path = "OU="+($user.departamento)+",OU="+($user.seccion)+",OU=ASM2122,DC=AINARA2021,DC=ES"
+    New-ADUser -Name $login -Path $path -SamAccountName $login -UserPrincipalName ($login+"@AINARA.ES") `
+    -AccountPassword (ConvertTo-SecureString "AINARA2021.es" -AsPlainText -Force) `
+    -GivenName $login -ChangePasswordAtLogon $false -Enabled $true
+
+    $group = Get-ADGroup ("CN="+$user.departamento+",OU="+$user.departamento+",OU="+$user.seccion+",OU=ASM2122,DC=AINARA2021,DC=ES")
+    $member = "CN="+$login+","+$path
+    Add-ADGroupMember $group -Members $member
+}
