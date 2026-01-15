6efef3c8fc75bb6eabb1b5a6d6be27a5d1d0df98
Ainara Serna Méndez <117570048+AinaraSerna@users.noreply.github.com>
2026-01-15 14:21:30 +0100

    Fix organizational unit path and update group commands

--- 1-secciones.ps1
+++ 1-secciones.ps1
@@ -1,18 +1,19 @@
 ﻿
 Set-Location "F:\Ensayos 2"
-New-ADOrganizationalUnit -Name "ASM2122" -Path "DC=AINARA2021,DC=ES" `
+New-ADOrganizationalUnit -Name "ASM2122" -Path "DC=AINARA,DC=ES" `
 -ProtectedFromAccidentalDeletion $false -Description "OU de ASM2122"
 
 New-ADGroup -Name "ASM2122" -GroupScope Global -Path "OU=ASM2122,DC=AINARA2021,DC=ES"
-$group = Get-ADGroup "CN=ASM2122,OU=ASM2122,DC=AINARA2021,DC=ES"
+$group = Get-ADGroup "CN=ASM2122,OU=ASM2122,DC=AINARA,DC=ES"
 
 $secciones= Import-Csv ".\secciones.csv"
 foreach($ou in $secciones){
     New-ADOrganizationalUnit -name ($ou.seccion) -Description ($ou.descripcion) `
-    -path "OU=ASM2122,DC=AINARA2021,DC=ES"
-    $path="OU="+($ou.seccion)+",OU=ASM2122,DC=AINARA2021,DC=ES"
+    -path "OU=ASM2122,DC=AINARA,DC=ES"
+    $path="OU="+($ou.seccion)+",OU=ASM2122,DC=AINARA,DC=ES"
 
     New-ADGroup -Name ($ou.seccion) -GroupScope Global -Path $path
     $member="CN="+($ou.seccion)+","+$path
     Add-ADGroupMember $group -Members $member
+
 }
