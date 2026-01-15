950363e87758934b7cc1e142fe3f77ce3e076b9c
Ainara Serna Méndez <117570048+AinaraSerna@users.noreply.github.com>
2026-01-15 14:21:56 +0100

    Fix formatting and update OU paths in script

--- 2-departamentos.ps1
+++ 2-departamentos.ps1
@@ -2,12 +2,13 @@
 Set-Location "F:\Ensayos 2"
 $departamentos = Import-Csv ".\departamentos.csv"
 foreach($ou in $departamentos){
-    $path="OU="+($ou.seccion)+",OU=ASM2122,DC=AINARA2021,DC=ES"
+    $path="OU="+($ou.seccion)+",OU=ASM2122,DC=AINARA,DC=ES"
     New-ADOrganizationalUnit -Name ($ou.departamento) -Path $path
-    $path="OU="+($ou.departamento)+",OU="+$ou.seccion+",OU=ASM2122,DC=AINARA2021,DC=ES"
+    $path="OU="+($ou.departamento)+",OU="+$ou.seccion+",OU=ASM2122,DC=AINARA,DC=ES"
 
     New-ADGroup -Name $ou.departamento -GroupScope Global -Path $path
-    $group = Get-ADGroup ("CN="+$ou.seccion+",OU="+$ou.seccion+",OU=ASM2122,DC=AINARA2021,DC=ES")
+    $group = Get-ADGroup ("CN="+$ou.seccion+",OU="+$ou.seccion+",OU=ASM2122,DC=AINARA,DC=ES")
     $member="CN="+($ou.departamento)+","+$path
     Add-ADGroupMember $group -Members $member
+
 }
